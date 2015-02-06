d541d9c72bb91a7aed5473ad7e2dae78e5ce0dcc
Jeffrey Amirault <jeffreyamirault@gmail.com>
2015-02-05 23:59:52 -0500

    introducing the superuser, Jef, brought to you by authorization
    super user has edit privelages for all users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,16 @@ module UsersHelper
   def user_signed_in?
     true
   end
+
+  def edit_username_field
+    form_for @user, remote: true do |f|
+      f.text_field :email
+    end
+  end
+
+  def destroy_user_link
+    confirmation = 'Are you sure you want to delete this user?'
+    link_to 'Delete User', user_path(@user), method: :delete, id: 'delete_user', data: { confirm: confirmation }
+  end
+
 end
