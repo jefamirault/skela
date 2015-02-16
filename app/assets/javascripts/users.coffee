8266746348fe83147fffac07602f59f2bd099b4a
Jeffrey Amirault <jeffreyamirault@gmail.com>
2015-02-16 10:36:47 -0500

    all kinds of users ajax

--- app/assets/javascripts/users.coffee
+++ app/assets/javascripts/users.coffee
@@ -3,9 +3,25 @@
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on 'click', '#user_select a.edit_user', ->
-  $('#user_form h1').html 'Edit User'
-  $('#user_form').removeClass 'hidden'
+#  $('#user_form h1').html 'Edit User'
+#  $('#user_form').removeClass 'hidden'
+  $('#user_form').addClass 'hidden'
 
 $(document).on 'click', '#user_select a.new_user', ->
-  $('#user_form h1').html 'New User'
-  $('#user_form').removeClass 'hidden'
+#  $('#user_form h1').html 'New User'
+  $('#user_form').addClass 'hidden'
+
+$(document).on 'click', '#delete_user', ->
+  user_id = $(this).data('user-id');
+  selector = 'tr[data-user-id=' + user_id + ']'
+
+  cloak_row = ->
+    cloak selector
+  cloak_row()
+
+  hide_row = ->
+    hide selector
+  window.setTimeout hide_row, 450
+
+cloak = (selector) ->
+  $(selector).addClass('invisible')
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,9 +5,27 @@ module UsersHelper
     end
   end
 
-  def destroy_user_link
+  def destroy_user_link(user = @user, text = 'Delete User')
     confirmation = nil
     # confirmation = 'Are you sure you want to delete this user?'
-    link_to 'Delete User', user_path(@user), method: :delete, id: 'delete_user', data: { confirm: confirmation }
+    link_to text, user_path(user), method: :delete, id: 'delete_user', data: { user_id: user.id, confirm: confirmation }, remote: true
+  end
+
+  def user_select_row(user, options = {})
+    klass = options[:hidden] ? 'hidden invisible' : ''
+    content_tag :tr, data: { user_id: user.id }, class: klass do
+      username_cell = content_tag :td do
+        content_tag :span, class: "user_#{user.id}_username" do
+          user.username
+        end
+      end
+      edit_cell = content_tag :td do
+        link_to 'Edit', edit_user_path(user), remote: true, class: 'edit_user'
+      end
+      destroy_cell = content_tag :td do
+        destroy_user_link user, 'Destroy'
+      end
+      (username_cell + edit_cell + destroy_cell).html_safe
+    end
   end
 end
