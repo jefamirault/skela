7b74ba32deaddc75b31c6bc7d99dd47c3f2ebb6e
Jeffrey Amirault <jeffreyamirault@gmail.com>
2015-07-13 17:03:46 -0400

    root to welcome new instead of login

--- app/assets/javascripts/ajax_navigation.coffee
+++ app/assets/javascripts/ajax_navigation.coffee
@@ -17,6 +17,7 @@ navigate = (section, parent_controller=section, push=true) ->
   $('body').attr 'class', parent_controller
   cloak '#container'
   if push
+    debugger
     window.history.pushState( {} , '', '/' + section );
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'login#new_session'
+  root 'welcome#index'
 
   get 'welcome' => 'welcome#index', as: :welcome
 
