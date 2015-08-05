b884b2066c71851203ed62f8d38b07c343ed0993
Jeffrey Amirault <jamiraul@umass.edu>
2015-08-05 09:26:50 -0400

    flash messages are the right size now to match the nav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,39 +16,6 @@
 //= require jquery-ui/autocomplete
 //= require_tree .
 
-function doc_keyUp(e) {
-    if (e.shiftKey && e.keyCode == 40) {
-        $('.developer_pad').toggleClass('hidden');
-    } else if (e.keyCode == 32) {
-        hideFlash();
-    }
-}
-document.addEventListener('keyup', doc_keyUp, false);
-
-function hideFlash() {
-    if ($('input:focus').size() == 0)
-        $('.flash').addClass('hidden');
-}
-function flashNotice(message) {
-    if (message == '')
-        return false;
-    $('.flash.notice').html(message);
-    $('.flash.alert').html('');
-    $('.flash.notice').removeClass('hidden');
-    window.setTimeout(function(){
-        $('.flash.notice').addClass('hidden');
-    }, 1800);
-}
-function flashAlert(message) {
-    if (message == '')
-        return false;
-    $('.flash.alert').html(message);
-    $('.flash.notice').html('');
-    $('.flash.alert').removeClass('hidden');
-    window.setTimeout(function(){
-        $('.flash.alert').addClass('hidden');
-    }, 1800);
-}
 
 $(document).on('click', '#trigger_flash_notice', function(event) {
     $('.flash.notice').toggleClass('hidden');
