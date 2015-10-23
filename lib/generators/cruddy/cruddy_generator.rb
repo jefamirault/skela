a5096a6d9e3a8e74ea0f175a0383ffa7345541ce
Jeffrey Amirault <jeffreyamirault@gmail.com>
2015-10-23 17:55:47 -0400

    cruddy generating views

--- lib/generators/cruddy/cruddy_generator.rb
+++ lib/generators/cruddy/cruddy_generator.rb
@@ -3,10 +3,14 @@ class CruddyGenerator < Rails::Generators::NamedBase
 
 
   def create_controllers
-    # copy_file 'cruddy_controller.rb', 'app/controllers/cruddy_controller.rb'
+    copy_file 'cruddy_controller.rb', 'app/controllers/cruddy_controller.rb'
     copy_file 'crud_controller.rb', "app/controllers/#{name}_controller.rb"
     gsub_file "app/controllers/#{name}_controller.rb", /(CrudController)/, "#{name.titleize}Controller"
     gsub_file "app/controllers/#{name}_controller.rb", /(:crud)/, ":#{name.singularize}"
   end
-  
+
+  def create_views
+    directory 'cruddy', 'app/views/cruddy'
+    directory 'crud', "app/views/#{name}"
+  end
 end
