a6ce76d4edcaedbb964d1bc070c93e2ac14d7ca4
Jeffrey Amirault <jeffreyamirault@gmail.com>
2015-10-23 16:11:43 -0400

    cruddy generating!

--- lib/generators/cruddy/cruddy_generator.rb
+++ lib/generators/cruddy/cruddy_generator.rb
@@ -2,5 +2,8 @@ class CruddyGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   argument :layout_name, type: :string, default: 'crud'
 
-  copy_file
+  def create_controllers
+    copy_file 'cruddy_controller.rb', 'app/controllers/cruddy_controller.rb'
+  end
+  
 end
