d1ee359c5d2a1f28daf86f9d2c06962fa6118c36
Jeffrey Amirault <jeffreyamirault@gmail.com>
2016-02-23 16:35:27 -0500

    cleaned out old routes

--- config/routes.rb
+++ config/routes.rb
@@ -33,38 +33,8 @@ Rails.application.routes.draw do
       delete 'remove_file', to: 'resources#remove_file', as: 'remove_file'
     end
   end
-  
-  resources :trackers
-
-  resources :sounds
-
-  get 'calculators/budget'
-  get 'calculators/tip'
-
-  post 'quests/:id/new_challenge', to: 'quests#new_challenge', as: 'quests_new_challenge'
-  get 'quests/:id/play', to: 'quests#play', as: 'play_quest'
-  resources :quests
-
-  post 'quests/:id/attempt_challenge/:challenge_id', to: 'quests#attempt_challenge', as: 'attempt_challenge'
-  resources :challenges
 
 
-  get 'items/autocomplete', to: 'items#autocomplete', as: 'autocomplete_items'
-  resources :items
-
-  resources :inventories do
-    delete 'remove_item/:item_id', to: 'inventories#remove_item', as: 'remove_item'
-    post 'track_item', to: 'inventories#track_item', as: 'track_item'
-
-    post 'update_stock', to: 'inventories#update_stock', as: 'update_stock'
-  end
-
-  get 'tasks/autocomplete', to: 'tasks#autocomplete', as: 'autocomplete_tasks'
-  resources :tasks
-  get 'new_task', to: 'tasks#new'
-
-  resources :shifts
-  get 'new_shift', to: 'shifts#new'
 
   get 'login', to: 'login#new_session', as: 'login'
 
@@ -74,28 +44,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'login#destroy_session', as: 'logout'
   get 'logout', to: 'login#destroy_session'
 
-  resources :log_entries
 
-  resources :plants
-
-  resources :games, only: :index
-  get 'games/tic_tac_toe'
-  get 'games/hi_lo'
-  get 'games/hundred_puzzle'
-  get 'games/plane_of_worlds'
-  get 'open_portal_path/:direction', to: 'games#open_portal', as: 'open_portal'
-  get 'travel_portal_path/:direction', to: 'games#travel_portal', as: 'travel_portal'
-  resources :worlds
-
-  resources :purchases
-  delete 'purchases/:id/remove_purchase_link', to: 'purchases#remove_purchase_link', as: 'remove_purchase_link'
-
-
-  resources :issues do
-    delete 'remove_task/:task_id', to: 'issues#remove_task', as: 'remove_task'
-    post 'new_task'
-    post 'add_task', to: 'issues#add_task', as: 'add_task'
-  end
 
   get 'users/autocomplete', to: 'users#autocomplete', as: 'autocomplete_users'
   resources :users
@@ -107,66 +56,18 @@ Rails.application.routes.draw do
   post 'admin_create_user', to: 'users#admin_create', as: 'admin_create_user'
   get 'my_profile', to: 'users#my_profile', as: 'my_profile'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+
+
   root 'courses#index'
 
   get 'welcome' => 'welcome#index', as: :welcome
 
   get 'not_allowed' => 'users#not_allowed', as: :not_allowed
 
-  post 'toggle_header', to: 'user_settings#toggle_header', as: 'toggle_header'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 
   match '*path', to: redirect('/'), via: :all
 
