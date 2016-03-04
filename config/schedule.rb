0adf165cd4c88e3f99d893fa48ac924d3e404f91
Jeffrey Amirault <jeffreyamirault@gmail.com>
2016-03-04 14:18:25 -0500

    time whenever cron shit fuck

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,8 @@
 
 # Learn more: http://github.com/javan/whenever
 
+job_type :rake,    "cd :path && :environment_variable=:environment bundle exec rake -e :environment :task --silent :output"
+
 set :output, { error: 'error.log', standard: 'cron.log' }
 
 every 1.minute do
