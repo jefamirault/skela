91dd8fdbb2daa528c1102defea62cbbca4ebe9ce
Jeffrey Amirault <jeffreyamirault@gmail.com>
2016-04-10 17:22:14 -0400

    minimum viable product - search!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
   def search
-    @assignments = Assignment.ransack params[:q]
-    @exams       = Exam.ransack params[:q]
-    @readings    = Reading.ransack params[:q]
-    @resources   = Resource.ransack params[:q]
+    ftext = params[:ftext] || ''
+    @q = { course_user_id_eq: current_user.id, }
+    @assignments = Assignment.ransack(@q.merge(title_or_description_cont: ftext )).result
+    @exams       = Exam.ransack(@q.merge(topic_or_description_cont: ftext )).result
+    @readings    = Reading.ransack(@q.merge(topic_or_description_cont: ftext )).result
+    @resources   = Resource.ransack(@q.merge(description_cont: ftext )).result
   end
 end
