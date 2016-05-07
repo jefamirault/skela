a830d289a696a5d7e6962bb9a36d36a2a5354393
Jeffrey Amirault <jeffreyamirault@gmail.com>
2016-05-07 18:46:03 -0400

    fixed search and added secret focus=category feature

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,20 @@ class SearchController < ApplicationController
   def search
     ftext = params[:ftext] || ''
     @q = { course_user_id_eq: current_user.id, }
-    @assignments = Assignment.ransack(@q.merge(title_or_description_cont: ftext )).result
-    @exams       = Exam.ransack(@q.merge(topic_or_description_cont: ftext )).result
-    @readings    = Reading.ransack(@q.merge(topic_or_description_cont: ftext )).result
-    @resources   = Resource.ransack(@q.merge(description_cont: ftext )).result
+    assignments = Assignment.ransack(@q.merge(title_or_notes_cont: ftext )).result
+    exams       = Exam.ransack(@q.merge(title_or_notes_cont: ftext )).result
+    readings    = Reading.ransack(@q.merge(topic_or_notes_cont: ftext )).result
+    resources   = Resource.ransack(@q.merge(title_cont: ftext )).result
+
+    @groups = {
+        assignments: assignments,
+        exams:       exams,
+        readings:    readings,
+        resources:   resources
+    }
+    # binding.pry
+    if params[:focus]
+      @groups = { params[:focus].to_sym => @groups[params[:focus].to_sym] }
+    end
   end
 end
