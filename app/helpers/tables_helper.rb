c8ef3636a77bb5639142c93e78200ed3abd033c4
Jeffrey Amirault <jeffreyamirault@gmail.com>
2016-06-02 15:23:41 -0400

    Slimmed down tables helper and refactored megatable

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -4,13 +4,9 @@ module TablesHelper
     content_tag :table, id: model_name.pluralize do
       header = content_tag :thead do
         content_tag :tr do
-          cols = ''
-          fields.each do |field|
-            formatted_field = field.titleize
-
-            cols << content_tag(:th, formatted_field, data: { column: "#{model_name}_#{field}" })
-          end
-          cols.html_safe
+          fields.map do |field|
+            content_tag(:th, field.titleize, data: { column: "#{model_name}_#{field}" })
+          end.reduce(:+).html_safe
         end
       end
 
@@ -39,9 +35,8 @@ module TablesHelper
     edit_title = "Editing #{model_name.titleize}"
 
     content_tag :tr, data: { object_id: resource.id, swap_title: edit_title, fade_content: true }, class: klass do
-      cols = ''
-      fields.each do |field|
-        cols << content_tag(:td, data: { column: "#{model_name}_#{field}" }) do
+      fields.map do |field|
+        content_tag(:td, data: { column: "#{model_name}_#{field}" }) do
           content_tag(:span, class: "#{model_name}_#{resource.id}_#{field}") do
             value = resource.send(field)
             if value.class == Date
@@ -56,63 +51,8 @@ module TablesHelper
 
           end
         end
-      end
-
-      cols.html_safe
+      end.reduce(:+).html_safe
     end
   end
 
-  def supertable(resources, fields = {})
-    model = resources.first.class.to_s.downcase
-    content_tag :table, id: model.pluralize do
-      header = content_tag :thead do
-        content_tag :tr do
-          cols = ''
-          fields.map do |field, options|
-            formatted_field = field.titleize
-
-            cols << content_tag(:th, formatted_field, data: { column: "#{model}_#{field}" })
-          end
-          cols.html_safe
-        end
-      end
-
-      body = content_tag :tbody do
-        rows = ''
-        resources.each do |resource|
-          rows << superrow(resource, fields)
-        end
-        rows.html_safe
-      end
-
-      header + body
-    end
-  end
-
-  def superrow(resource, fields)
-    model = resource.class.to_s.downcase
-    klass = 'show_path'
-    klass << cycle(' odd', ' even')
-    content_tag :tr, data: { object_id: resource.id }, class: klass do
-      cols = ''
-      fields.map do |field, options|
-        field = field.downcase
-        cols << content_tag(:td, data: { column: "#{model}_#{field}" }) do
-          content_tag(:span, class: "#{model}_#{resource.id}_#{field}") do
-            value = resource.send(field)
-            if value.class == Date
-              value.strftime('%A, %B %d, %Y')
-            elsif value.class == Time
-              value.strftime('%-I:%M %p')
-            else
-              value.to_s
-            end
-
-          end
-        end
-      end
-
-      cols.html_safe
-    end
-  end
 end
