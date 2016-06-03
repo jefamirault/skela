c9bf5884d417d6d2cfd6feb6cdbfcac5c5af7b12
Jeffrey Amirault <jeffreyamirault@gmail.com>
2016-06-03 07:14:14 -0400

    remove useless variable assignment

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,6 @@ module UsersHelper
   end
 
   def destroy_user_link(user = @user, text = 'Delete User')
-    confirmation = nil
     confirmation = 'Are you sure you want to delete this user?'
     link_to text, user_path(user), method: :delete, id: 'delete_user', data: { user_id: user.id, confirm: confirmation, remote: true, fade_content: true, swap_title: 'Users' }
   end
